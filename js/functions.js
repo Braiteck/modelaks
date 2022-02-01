fc62443256648fe0279381cdc61485a514c470ef
Vitaliy Sulimenko <prevetal@gmail.com>
2022-02-01 12:37:53 +0200

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,127 @@
+$(() => {
+	// Есть ли поддержка тач событий или это apple устройство
+	if (!is_touch_device() || !/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) $('html').addClass('custom_scroll')
+
+
+	// Ленивая загрузка
+	setTimeout(() => {
+		observer = lozad('.lozad', {
+			rootMargin: '200px 0px',
+			threshold: 0,
+			loaded: el => el.classList.add('loaded')
+		})
+
+		observer.observe()
+	}, 200)
+
+
+	// Установка ширины стандартного скроллбара
+	$(':root').css('--scroll_width', widthScroll() + 'px')
+
+
+	// Маска ввода
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	// Увеличение картинки
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false,
+		},
+		Thumbs: {
+			autoStart: false,
+		}
+	})
+
+
+	// Плавная прокрутка к якорю
+	var locationHash = window.location.hash,
+		scrollOffset = 92
+
+	$('.scroll_btn').click(function (e) {
+		e.preventDefault()
+
+		let href = $(this).data('anchor')
+
+		if ($(this).data('offset')) scrollOffset = $(this).data('offset')
+
+		$('html, body').stop().animate({ scrollTop: $(href).offset().top - scrollOffset }, 1000)
+	})
+
+	if (locationHash) {
+		$('html, body').stop().animate({ scrollTop: $(locationHash).offset().top - scrollOffset }, 1000)
+	}
+
+
+	// Моб. версия
+	fiestResize = false
+
+	if ($(window).width() < 480) {
+		$('meta[name=viewport]').attr('content', 'width=480, user-scalable=no')
+
+		fiestResize = true
+	}
+})
+
+
+
+// Вспомогательные функции
+const setHeight = (className) => {
+	let maxheight = 0
+
+	className.each(function () {
+		const elHeight = $(this).outerHeight()
+
+		if (elHeight > maxheight) maxheight = elHeight
+	})
+
+	className.outerHeight(maxheight)
+}
+
+
+const is_touch_device = () => !!('ontouchstart' in window)
+
+
+const widthScroll = () => {
+	let div = document.createElement('div')
+
+	div.style.overflowY = 'scroll'
+	div.style.width = '50px'
+	div.style.height = '50px'
+	div.style.visibility = 'hidden'
+
+	document.body.appendChild(div)
+
+	let scrollWidth = div.offsetWidth - div.clientWidth
+	document.body.removeChild(div)
+
+	return scrollWidth
+}
---
+++ js/scripts.js
@@ -0,0 +1,135 @@
+$(() => {
+	// Ширина окна для ресайза
+	WW = $(window).width()
+
+
+	// Основной слайдер на главной
+	if ($('.main_slider .swiper-container').length) {
+		new Swiper('.main_slider .swiper-container', {
+			loop: true,
+			speed: 750,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			}
+		})
+	}
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click(function (e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			$(this).addClass('active')
+			$('header .menu').fadeIn(300)
+		} else {
+			$(this).removeClass('active')
+			$('header .menu').fadeOut(200)
+		}
+	})
+
+
+	// Фиксирпованная картинка
+	image = $('.fixed_img'),
+		stopBlock = $('.about_info'),
+		startOffset = $('header').outerHeight() + $('.main_slider').outerHeight() * 0.5 - image.height() * 0.5,
+		imageStopOffset = stopBlock.offset().top + stopBlock.outerHeight() * 0.5 - image.height() * 0.5
+
+	image.css('transform', 'translateY(' + startOffset + 'px)')
+
+	$(window).scrollTop() >= (imageStopOffset - startOffset)
+		? image.addClass('stop').css('transform', 'translateY(' + imageStopOffset + 'px)')
+		: image.removeClass('stop').css('transform', 'translateY(' + startOffset + 'px)')
+})
+
+
+
+$(window).on('load', () => {
+	// Фикс. шапка
+	headerInit = true,
+		headerHeight = $('header').outerHeight()
+
+	$('header').wrap('<div class="header_wrap"></div>')
+	$('.header_wrap').height(headerHeight)
+
+	headerInit && $(window).scrollTop() > 0
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+})
+
+
+
+$(window).on('resize', () => {
+	if (typeof WW !== 'undefined' && WW != $(window).width()) {
+		// Моб. версия
+		if (!fiestResize) {
+			$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1, maximum-scale=1')
+			if ($(window).width() < 480) $('meta[name=viewport]').attr('content', 'width=480, user-scalable=no')
+
+			fiestResize = true
+		} else {
+			fiestResize = false
+		}
+
+
+		// Фикс. шапка
+		headerInit = false
+		$('.header_wrap').height('auto')
+
+		setTimeout(() => {
+			headerInit = true
+			headerHeight = $('header').outerHeight()
+
+			$('.header_wrap').height(headerHeight)
+
+			headerInit && $(window).scrollTop() > 0
+				? $('header').addClass('fixed')
+				: $('header').removeClass('fixed')
+		}, 100)
+
+
+		// Перезапись ширины окна
+		WW = $(window).width()
+	}
+})
+
+
+$(window).on('scroll', () => {
+	// Фиксирпованная картинка
+	$(window).scrollTop() >= (imageStopOffset - startOffset)
+		? image.addClass('stop').css('transform', 'translateY(' + imageStopOffset + 'px)')
+		: image.removeClass('stop').css('transform', 'translateY(' + startOffset + 'px)')
+
+
+	// Фикс. шапка
+	typeof headerInit !== 'undefined' && headerInit && $(window).scrollTop() > 0
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+})
+
+
+
+// Анимация
+const boxes = document.querySelectorAll('.observer')
+
+function scrollTracking(entries) {
+	for (const entry of entries) {
+		if (entry.intersectionRatio >= 0.2) {
+			entry.target.classList.add('animated')
+		}
+	}
+}
+
+const observerClass = new IntersectionObserver(scrollTracking, {
+	threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+})
+
+boxes.forEach(element => observerClass.observe(element))
